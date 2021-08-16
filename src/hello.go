7d7b68c922da03907070faae04e67d31ed6fe716
ipek turgul <ipek.turgul@gmail.com>
2021-08-16 16:49:33 +1000

    implement next requirements

--- src/hello.go
+++ src/hello.go
@@ -2,10 +2,35 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+const french = "French"
+const spanish = "Spanish"
+
+// Public functions start with uppercase
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+// Private functions start with lowercase
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- src/hello_test.go
+++ src/hello_test.go
@@ -3,10 +3,32 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Peckles")
-	want := "Hello, Peckles"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// Indicates this method is  a helper
+		// Also fail message will point to line where this function was called
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Peckles", "")
+		want := "Hello, Peckles"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Franz", "French")
+		want := "Bonjour, Franz"
+		assertCorrectMessage(t, got, want)
+	})
 }
